e63fee5af6174fcc9d5d372406f4332974428a75
yxwuxuanl <wuxuanlinnn@163.com>
2023-07-06 16:16:30 +0800

    commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	"os"
+)
+
+var (
+	GroupName            = os.Getenv("GROUP_NAME")
+	CertManagerNamespace = os.Getenv("CERT_MANAGER_NAMESPACE")
+)
+
+func main() {
+	if GroupName == "" {
+		panic("GROUP_NAME must be specified")
+	}
+
+	if CertManagerNamespace == "" {
+		panic("CERT_MANAGER_NAMESPACE must be specified")
+	}
+
+	cmd.RunWebhookServer(GroupName, &LegoSolver{})
+}
